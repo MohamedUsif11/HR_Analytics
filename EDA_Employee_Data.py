e1181ea601ae39f555102c70eb30db249f11ea06
Mohamed Youssef Ali <126957564+MohamedUsif11@users.noreply.github.com>
2023-08-14 01:49:51 +0300

    Update EDA_Employee_Data.py

--- EDA_Employee_Data.py
+++ EDA_Employee_Data.py
@@ -6,6 +6,7 @@ selected_page = st.sidebar.radio("Navigate", ["Introduction", "Categorical Featu
 import pandas as pd
 import numpy as np
 import plotly.express as px
+import os
 
 # Read the CSV file
 csv_file_path = r'Uncleaned_employees_final_dataset (1).csv'
